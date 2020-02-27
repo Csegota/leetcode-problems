2f724c08635ea935669b1743b04130d5b2a23af0
Csegota <csegota406@gmail.com>
2020-02-27 12:13:25 -0800

    883 check in - 6h discovery

---
+++ ApproachMaster.md
@@ -0,0 +1,5 @@
+* Write down known global variables
+* Identify type? DP, Matrix, OOP, graph traversal BFS, DFS, string manipulation.
+* Come up with representation for the problem?
+* Get something running.
+* Simplify problem to more simple forms before moving on to more advanced. e.g. maybe it helps to first solve with a sorted array, instead of the unsorted on given. 
--- Easy/883 - Projection Area of 3D Shapes/Solution 1.js
+++ Easy/883 - Projection Area of 3D Shapes/Solution 1.js
@@ -34,74 +34,176 @@ var projectionArea = function(grid) {
 	const towers = n * n;
 
 	//variables
-	let x = 0;
-	let y = 0;
-	let z = 0;
 	let height = 0; //given 0-50
 	let xyProj = 0;
 	let xzProj = 0;
 	let yzProj = 0;
 
-	var myArr = new Array();
-	//memo
-	// key x,y,z = height;
-	switch(grid.length) {
-		case 1:
-			break;
-		case 2:
-			myArr[0] = new Array();
-			break;
-		case 3:
-			myArr[0][0] = new Array();
-			break;
-		default:
-	}
-	// var myArr = new Array();
-	// myArr[0] = new Array();
-	// myArr[0][0] = new Array();
+	//Create 2D matrix Representation of the input.
+	//Initialize values.
+	var repArr = zeros([n,grid[0].length]);
 
-	while (x<n) {
+	//var repArr = new Array();
+	//repArr[0] = new Array();
+		
+	// //Initialize array values to 0.
+	// let i = 0;
+	// let j = 0;
+	// while (j < n-1) {
+	// 	while (i < n-1) {
+	// 		repArr[i][j]= 0;
+	// 		i++
+	// 	}
+	// 	j++
+	// }
+
+	//Initialize Memo Array
+	//Key - arr[x][y][z] = v; v = height of tower at position.
+
+	//var memoArr = zeros([n,grid[0].length]);				//Recursive Initialization
+	var memoArr = Array(n).fill().map(() 					//Using map & fill functions.
+					=> Array(grid[0].length).fill(0));
+
+	//Set the 'v' values in the memo array;
+	let x = 0;
+	let y = 0;
+	let z = 0;
+	while (z<n) {
 		while (y<n) {
-			while (z<n) {
-				myArr[i][j][k] = grid[i][j][k];
-				z++;
+			while (x<n) {
+				memoArray[x][y][z] = getTower();
+				x++;
 			}
 			y++;
 		}
-		x++;
+		z++;
 	}
-
-	// state
-	//myArr[x][y][z] = height
-
-	//relation
+	/* 
+	[0][0][0] x++ 
+	[1][0][0] y++
+	[1][1][0] z++
+	[1][1][1] x++
+	[2][1][1]
+	[2][2][1]
+	[2][2][2]
+	[3][2][2]
+	[3][3][2]
+	[3][3][3]
+	*/
+	//Lets think about this DP wise, now that we understand the problem a little better.
+	//I'm starting to think that there is a relationship between the projections and n. where n is significant because each level adds another layer.
+	//n = 1 is a single array, single value input
+	//n = 2 is a double array, double value input
+	//n = 3 is a triple array, tripple input.
+	//Now I need to find the relationship between an xy, xz, yz projection and its n (magnitude?)... something like n(x) = greaterOf( n(x-1), n(x-2) )
+	//
 
 
 	return xyProj + xzProj + yzProj;
 
-	//High Level
-	//
-
-	//Think of the values v = grid[i][j] or grid[i][j][z] as the coordinates of the start of the block, centered on the point and alligned with axi
-	//Therefor, a grid with only x y values. will always have all its towers starting at 0 on the z axis, because z = 0, assumed.
-	//If we add a z value, the tower will be shifted up, away from the x=y plane.
-
-	//Example 1
-	// Input: [[2]]
-	// Output: 5
-	// grid[1][0][0] = v = 2
-	// Block height = 2
-	// Block location = [1][0][0]
-	// xyProj = 1
-	// xzProj = 2
-	// yzProj = 2
-	// area = 5
-
-	//Example 2
-	// Input: [[1,2],[3,4]]
-	// Output: 17
-    // grid[1][1][0] = v = 1
-    // grid[1][2][0] = v = 2
-    // grid[2][1][0] = v = 3
-    // grid[2][2][0] = v = 4
+	
 };
+
+//Initialize an array with 'dimensions' [a,b] where a = number of sub arrays, b = number of elements in each sub array.
+//Uses recursion, have not fully followed execution of this, code seems to obscure actual execution.
+function zeros(dimensions) {
+    var array = [];
+
+    for (var i = 0; i < dimensions[0]; ++i) {
+        array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
+    }
+
+    return array;
+};
+
+/*
+Lessons Learned:
+	* Come up with a prepresentation of the problem, especially true with array manipulation problems. In this case we can create a 2D array to represent the block locations at (x,y) and store their height z.
+	* Once I was able to look at the problem represented in 2D Array form, calculating the projection areas became very obvious.
+	* The xyProjArea = count locations that have a value != 0
+	* xzProj = sum max value for each column - or thinking about it from a more visual perspective, you know the highest value in each columnn will be the most 'significant'
+	* yzProj = sum max value for each row - ""
+
+Problem Discovery Notes / Workflow:
+
+	Think of the values v = grid[i][j] or grid[i][j][z] as the coordinates of the start of the block, centered on the point and alligned with axi
+	Therefor, a grid with only x y values. will always have all its towers starting at 0 on the z axis, because z = 0, assumed.
+	If we add a z value, the tower will be shifted up, away from the x=y plane.
+
+	Example 1
+	 Input: [[2]]
+	 Output: 5
+	
+	 [0][0]				//This grid represneation was added after 'breakthrouh' moment.
+	 [2][0]
+	
+	 grid[1][0][0] = v = 2		
+	 Block height = 2				
+	Block location = [1][0][0]	
+	xyProj = 1					
+	xzProj = 2					
+	yzProj = 2					
+	area = 5						
+
+	Example 2
+	Input: [[1,2],[3,4]]
+	Output: 17
+	
+	[2][4]				//This grid represnetation was added after 'breakthrouh' moment.
+	[1][3]
+	
+	Blocks = 4 = n * n 			
+    grid[1][1][0] = v = 1		
+    grid[1][2][0] = v = 2		
+    grid[2][1][0] = v = 3		
+    grid[2][2][0] = v = 4		
+    xyProj = n * n
+  
+	xzProj = 6 = z value (vertical?) dictates?
+		width = x max
+		height = z max
+		projection area = tower1(greaterZOf([x1,z1],[x1,z2]) + tower2(greaterOf([x2,z1],[x2,z1]))
+		Observation - projection of xz plane concerns only x and z values.
+		Further - z seems to dictate tower height
+	yzProj = 7 = z value (vertical?) dictates?
+		width = 2 = n = y
+		height = greaterOf([y1,z1],[y1,z2]
+
+	Example 3
+	Input: [[1,0],[0,2]]
+	Output: 8
+	
+	[0][2]				//This grid represnetation was added after 'breakthrouh' moment.
+	[1][0]
+	
+	xyProj = 2
+	xzProj = 3
+	yzProj = 3
+	
+	Blocks = 4 = n * n
+	grid[1][1][0] = 1
+	grid[1][2][0] = 0
+	grid[2][1][0] = 0
+	grid[2][2][0] = 2
+	
+	xyProj = area1(greaterYof([x1,y1],[x1,y2])) + area2(greaterYof([x2,y1],[x2,y2]) = 2 + 2 = 4
+	xzProj = area1(greaterZof(0,0) + area2(greaterZof(0,0)) = 0
+	yzProj = area1(greaterZof())
+
+	 
+	THE BREAKTHROUGH MOMENT ~4-5h into "discovery"
+	 
+	Towers at location
+	0,0 - height 1
+	0,1 - height 0
+	1,0 - height 0 		[0][2]
+	1,1 - height 2		[1][0]
+	xyProjArea = straight on = count grid locations where value != 0;
+	xzProjArea = bottom to top greaters = 3
+	yzProjArea = left to right = 3
+	I think we just had a breakthrough.
+
+	If we build a matrix that represents the locations of the towers as x,y, then the value contained represents the height or z
+	Still not sure how I'm going to use DP for this, but I think I'm getting closer.
+*/
+
